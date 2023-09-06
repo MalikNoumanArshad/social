0153041b44ee173ca2fde1f1b86ad03688c3465a
MalikNoumanArshad <maliknoumanarshad@gmail.com>
2023-09-06 16:33:53 +0500

    add more changes 06/09/2023 2

--- src/components/formValidation.jsx
+++ src/components/formValidation.jsx
@@ -5,10 +5,6 @@ export const userSchema = Yup.object().shape({
     name: Yup.string().required().min(4),
     email: Yup.string().email().required(),
     password: Yup.string().min(4).max(10).required(),
-    phoneNumber: Yup.string()
-        .required("required")
-        .matches(phoneRegExp, 'Phone number is not valid')
-        .min(10, "Enter Complete number +923410536480")
-        .max(12, "Enter Phone number +923410536480"),
+    phoneNumber: Yup.string().required("required").matches(phoneRegExp, 'Phone number is not valid').min(10, "Enter Complete number +923410536480").max(12, "Enter Phone number +923410536480"),
     country: Yup.string().required("Select City").typeError("Select Country"),
 })
